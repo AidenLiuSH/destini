28198bb95253b71963378bf4ce12ce952f507d4c
AidenLiuSH <aidenliush@gmail.com>
2022-05-23 18:48:39 -0400

    finished project

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -1,6 +1,9 @@
 import 'story.dart';
 
 class StoryBrain {
+  //track which story the user is currently viewing.
+  int _storyNumber = 0;
+  //Story bank
   final List<Story> _storyData = [
     Story(
         storyTitle:
@@ -34,32 +37,52 @@ class StoryBrain {
   ];
   //returns the first storyTitle from _storyData.
   String getStory() {
-    return _storyData[0].storyTitle;
+    return _storyData[_storyNumber].storyTitle;
   }
 
   //returns the text for the first choice1 from _storyData.
   String getChoice1() {
-    return _storyData[0].choice1;
+    return _storyData[_storyNumber].choice1;
   }
 
   //returns the text for the first choice2 from _storyData.
   String getChoice2() {
-    return _storyData[0].choice2;
+    return _storyData[_storyNumber].choice2;
+  }
+
+  //Progress to the next story, takes in choice made by user
+  void nextStory(int choiceNumber) {
+    print(_storyNumber);
+    print(choiceNumber);
+    if (_storyNumber == 3 || _storyNumber == 4 || _storyNumber == 5) {
+      restart();
+    } else {
+      if ((_storyNumber == 0 && choiceNumber == 1) ||
+          (_storyNumber == 1 && choiceNumber == 1)) {
+        _storyNumber = 2;
+      } else if (_storyNumber == 0 && choiceNumber == 2) {
+        _storyNumber = 1;
+      } else if (_storyNumber == 2 && choiceNumber == 1) {
+        _storyNumber = 5;
+      } else if (_storyNumber == 2 && choiceNumber == 2) {
+        _storyNumber = 4;
+      } else if (_storyNumber == 1 && choiceNumber == 2) {
+        _storyNumber = 3;
+      }
+    }
+  }
+
+  //restart the story number to 0 if curr story number is 3,4 or 5.
+  void restart() {
+    _storyNumber = 0;
+  }
+
+  //Button only visible of story number is 0,1 or 2
+  bool buttonVisible() {
+    if (_storyNumber == 0 || _storyNumber == 1 || _storyNumber == 2) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
-
-//TODO: Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.
-
-//TODO: Step 25 - Change the storyNumber property into a private property so that only story_brain.dart has access to it. You can do this by right clicking on the name (storyNumber) and selecting Refactor -> Rename to make the change across all the places where it's used.
-
-//TODO: Step 16 - Create a property called storyNumber which starts with a value of 0. This will be used to track which story the user is currently viewing.
-
-//TODO: Step 17 - Create a method called nextStory(), it should not have any outputs but it should have 1 input called choiceNumber which will be the choice number (int) made by the user.
-
-//TODO: Step 20 - Download the story plan here: https://drive.google.com/uc?export=download&id=1KU6EghkO9Hf2hRM0756xFHgNaZyGCou3
-
-//TODO: Step 21 - Using the story plan, update nextStory() to change the storyNumber depending on the choice made by the user. e.g. if choiceNumber was equal to 1 and the storyNumber is 0, the storyNumber should become 2.
-
-//TODO: Step 22 - In nextStory() if the storyNumber is equal to 3 or 4 or 5, that means it's the end of the game and it should call a method called restart() that resets the storyNumber to 0.
-
-//TODO: Step 27 - Create a method called buttonShouldBeVisible() which checks to see if storyNumber is 0 or 1 or 2 (when both buttons should show choices) and return true if that is the case, else it should return false.
